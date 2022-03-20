1382bbf8cf5bf666a5d2cde377d92e7a01dda999
Zero33w <guillem46723@gmail.com>
2022-03-20 23:17:20 +0100

    Implementada clase Program

--- Torres_de_Hanoi/Program.cs
+++ Torres_de_Hanoi/Program.cs
@@ -10,9 +10,51 @@ namespace Torres_de_Hanoi
     {
         static void Main(string[] args)
         {
+            //Inicializo las pilas
+            Pila ini = new Pila("Inicio");
+            Pila fin = new Pila("Final");
+            Pila aux = new Pila("Auxiliar");
 
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            //Inicializa un objeto de la clase hanoi
+            Hanoi hanoi = new Hanoi();
+            bool IoR = false;
+
+            Console.WriteLine("Inserta 1 para el modo Iterarivo o cualquier otro caracter para el modo Recursivo");
+            string ir = Console.ReadLine();
+            if (ir == "1")
+            {
+                IoR = true;
+            }
+            else
+            {
+                IoR = false;
+            }
+            //El programa te pide que le des un valor para saber el número de discos que usarás
+            Console.WriteLine("Inserta un entero");
+            string str = Console.ReadLine();
+            int n = int.Parse(str);
+
+            //Segun el valor establecido se añadirán discos en la pila ini
+            for (int i = 0; i < n; i++)
+            {
+                Disco d = new Disco(n - i);
+                ini.push(d);
+            }
+
+            //Llama a la función que calculará los movimientos necesarios y muestra el resultado en consola
+
+            if (IoR)
+            {
+                int resI = hanoi.iterativo(n, ini, fin, aux);
+                Console.WriteLine("Necesitas mínimo " + resI + " movimientos");
+            }
+            else
+            {
+                int resR = hanoi.algoritmo_recursivo(n, ini, fin, aux);
+                Console.WriteLine("Necesitas mínimo " + resR + " movimientos");
+            }
+
+            //Esto es para que no se cierre la consola sin que te de tiempo a revisar el resultado
             Console.ReadKey();
         }
     }
